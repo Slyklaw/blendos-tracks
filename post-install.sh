9c9f3b31f10e3f9a09f9db5cfb09cd7c614bed3d
Slyklaw Dev <slyklaw@gmail.com>
2025-12-21 01:54:52 -0800

    post-install

---
+++ post-install.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+gsettings set org.cinnamon.theme name "Materia-dark"
+gsettings set org.cinnamon.desktop.session idle-delay 0
+gsettings set org.cinnamon.desktop.interface gtk-theme "Materia-dark"
+gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/gnome/blobs-l.svg"
+gsettings set org.x.apps.portal color-scheme "prefer-dark"
+gsettings set org.cinnamon.desktop.interface icon-theme "Papirus-dark"
