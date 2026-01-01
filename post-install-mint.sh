fe50700583ed8d3278df9b3d065662038a36321c
Slyklaw Dev <slyklaw@gmail.com>
2026-01-01 00:17:43 -0800

    post-install.sh typos

--- post-install-mint.sh
+++ post-install-mint.sh
@@ -1,20 +1,20 @@
 #/bin/sh
 
 gsettings set org.cinnamon.desktop.session idle-delay 0
+gsettings set org.x.apps.portal color-scheme "prefer-dark"
+gsettings set org.cinnamon.theme name "Mint-Y-Dark"
+gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
 
-# gsettings set org.x.apps.portal color-scheme "prefer-dark"
-# gsettings set org.cinnamon.theme name "Materia-dark"
-# gsettings set org.cinnamon.desktop.interface gtk-theme "Materia-dark"
 # gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/gnome/blobs-l.svg"
 # gsettings set org.cinnamon.desktop.interface icon-theme "Papirus-dark"
 
 flatpak install -y com.brave.Browser
 flatpak install -y com.discordapp.Discord
-flatpak install -y com.github.tchx85.Flatseal
-flatpak install -y com.gimp.GIMP
+flatpak install -y com.github.tchx84.Flatseal
+flatpak install -y org.gimp.GIMP
 flatpak install -y com.heroicgameslauncher.hgl
 flatpak install -y com.lunarclient.LunarClient
-flatpak install -y io.missioncenter.MissionControl
+flatpak install -y io.missioncenter.MissionCenter
 flatpak install -y com.valvesoftware.Steam
 flatpak install -y com.vscodium.codium
 
